7bc7f81255dd6f558d92b3a300a6d08d88f2fcbc
raphael liyanage <raphael.kliyanage@gmail.com>
2024-02-26 19:08:50 +0100

    fix(calculus.asm): correct algorithm

--- calculus/calculus.asm
+++ calculus/calculus.asm
@@ -1,29 +1,38 @@
 # x86 assembly: calculus
+# inspiration: https://stackoverflow.com/questions/28524535/add-2-numbers-and-print-the-result-using-assembly-x86
 # code's under MIT License
 
+.section .text
 .global _start
-.intel_syntax
-
-.section .data
-a:
-	.int 22
-b:
-	.int 44
-c:
-	.int 0
-
 _start:
-	# loading variables
-	mov %eax, [a]
-	mov %ebx, [b]
-	
-	# addition
-	add %eax, %ebx
-	
-	# storing on [c]
-	mov [c], %eax
-	
-	# "exit" syscall
-	mov %eax, 0x01	# "exit"
-	mov %ebx, [c]	# return c's value
-	int 0x80	# execute syscall
+	mov %eax, 0x04	# 4
+	mov %ebx, 0x04	# 4
+	add %eax, %ebx	# eax+=ebx (eax=eax+ebx=4+4=8)
+
+	# converting eax to ascii and storing it onto the stack
+	sub %esp, 0x10	# reserve space on the stack
+	mov %ecx, 0x0a
+	mov %ebx, 0x10
+
+	.L1:
+	xor %edx, %edx	# Don't forget it! (why?)
+	div %ecx		# extract the last decimal digit
+	or %dl, 0x30	# convert remainder to ascii
+	sub %ebx, 1
+	mov [%esp+%ebx], %dl	# store remainder on the stack (reverse order)
+	test %eax, %eax	# until there is nothing to divide
+	jnz .L1
+
+	mov %eax, 0x04	# sys_write
+	lea %ecx, [%esp+%ebx]	; pointer to the frist ascii digit
+	mov %edx, 0x01
+	sub %edx, %ebx	# count of digits
+	mov %ebx, 0x01	# stdout
+	int 0x80	# call 32bit linux
+
+	add %esp, 16	# restore the stack
+
+	mov %eax, 0x01	# sys_exit
+	xor %ebx, %ebx	# return value
+	int 0x80	# call 32bit linux
+
